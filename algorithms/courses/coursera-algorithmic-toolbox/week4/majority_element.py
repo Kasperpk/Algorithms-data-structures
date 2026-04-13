00eb9b4a63a406e2700f372368b93a250ea93375
Kasper <Kasperkpedersen93@gmail.com>
2026-04-13 03:27:40 +0200

    majority element

--- algorithms/courses/coursera-algorithmic-toolbox/week4/majority_element.py
+++ algorithms/courses/coursera-algorithmic-toolbox/week4/majority_element.py
@@ -1,9 +1,14 @@
 def majority_element(numbers):
-    for number in numbers:
-        if numbers.count(number) > int(len(numbers)/2):
-            return 1
-        return 0
 
+    numbers_count = {number: 0 for number in numbers}
+
+    for number in numbers:
+        numbers_count[number] += 1
+
+    for number in numbers:
+        if numbers_count[number] > int(len(numbers)/2):
+            return 1
+    return 0
 
 if __name__ == '__main__':
     n = int(input())
